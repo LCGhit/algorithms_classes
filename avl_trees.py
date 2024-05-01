863c1b355a4eaf1ff5781b4d05d70d540a841865
LCGhit <luispcunha@proton.me>
2024-05-01 21:50:53 +0100

    balance avl tree function

--- avl_trees.py
+++ avl_trees.py
@@ -16,3 +16,24 @@ def RotateLeft(B):
     B.dir = A.esq
     A.esq = B
     return A
+
+def fixRight(A):
+    B = A.dir
+    if(B.bal == -1):
+        A.bal = B.bal = 0
+        A = RotateLeft(A)
+    else:
+        C = B.esq
+        match C.bal:
+            case 1:
+                A.bal = 0
+                B.bal = -1
+            case -1:
+                A.bal = 1
+                B.bal = 0
+            case 0:
+                A.bal = B.bal = 0
+        C.bal = 0
+        A.dir = RotateRight(B)
+        A = RotateLeft(A)
+    return A
